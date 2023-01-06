4a68498da0107b051e331b93216a120a2cf285b2
Ludwig Pereira <ludwig.m.pereira@gmail.com>
2023-01-06 15:24:12 +0100

    first solution

---
+++ kata5.js
@@ -0,0 +1,16 @@
+let XO = (arg) => {
+let newArg = arg.toLowerCase();
+let numO = 0;
+let numX = 0;
+for (let i in newArg) {
+  if (newArg[i]==="o") {numO++}
+  if (newArg[i]==="x") {numX++}
+}
+return (numO===numX)?true:false
+}
+console.log(XO("vdfgeoxoxxoox"))
+console.log(XO("ooxx")) 
+console.log(XO("xooxx")) 
+console.log(XO("ooxXm")) 
+console.log(XO("zpzpzpp"))  // when no 'x' and 'o' is present should return true
+console.log(XO("zzoo")) 
