acf8df9b27e12ff7dd1243f18118a4344c3ba14e
Ludwig Pereira <ludwig.m.pereira@gmail.com>
2023-01-06 15:56:02 +0100

    second solution

--- kata5.js
+++ kata5.js
@@ -14,3 +14,18 @@ console.log(XO("xooxx"))
 console.log(XO("ooxXm")) 
 console.log(XO("zpzpzpp"))  // when no 'x' and 'o' is present should return true
 console.log(XO("zzoo")) 
+
+
+let XO2 = (str) => {
+console.log("xoxo".toLowerCase().split("x"))
+console.log("xoxo".toLowerCase().split("o"))
+
+return str.toLowerCase().split("x").length===str.toLowerCase().split("o").length
+}
+
+console.log(XO2("vdfgeoxoxxoox"))
+console.log(XO("ooxx")) 
+console.log(XO("xooxx")) 
+console.log(XO("ooxXm")) 
+console.log(XO("zpzpzpp"))  // when no 'x' and 'o' is present should return true
+console.log(XO("zzoo")) 
